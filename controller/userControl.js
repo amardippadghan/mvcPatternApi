08ed2d527fafead69d6d4d0389b4e12f94d8f083
amardip padghan <82278378+amardippadghan@users.noreply.github.com>
2024-02-02 11:39:42 +0530

    adding password encrption and login

--- controller/userControl.js
+++ controller/userControl.js
@@ -2,21 +2,27 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const User = require("../models/users");
 const nodemailer = require("nodemailer");
+const bcrypt = require("bcrypt");
 
 const key = process.env.JWT_SECRET_KEY;
-
-
+const saltRound = 10 ; 
 
 const creatUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
     console.log("Received user data:", { name, email, password });
+      console.log("Salt rounds:", saltRound);
+    const hashedPassword = await bcrypt.hash(password, saltRound);
+    
+    console.log("Hashed password:", hashedPassword);
 
-    const token = jwt.sign({ name, email, password }, key, { expiresIn: "3h" });
+    const token = jwt.sign({ name, email, password: hashedPassword }, key, {
+      expiresIn: "3h",
+    });
     console.log("Generated JWT token:", token);
 
-    const user = await User.create({ name, email, password });
+    const user = await User.create({ name, email, password: hashedPassword });
     console.log("Created user:", user);
 
     res.status(201).json({ user: user, token: token });
@@ -26,47 +32,64 @@ const creatUser = async (req, res) => {
   }
 };
 
-const SendMail = async (req, res) => { 
-   try {
-     console.log("Request body:", req.body);
-     const { email } = req.body;
-     console.log("Recipient email:", email);
+const Login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    console.log("User found:", user);
+    if (!user) {
+      res.status(400).json({ error: "User not found" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
 
-     const transporter = nodemailer.createTransport({
-       host: "smtp.ethereal.email",
-       port: 587,
-       auth: {
-         user: "allene.goyette@ethereal.email",
-         pass: "xNPJDm61NYRDSYAw6c",
-       },
-     });
+    if (!isMatch) {
+      res.status(400).json({ error: "Invalid password" });
+    }
+    res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    console.error("Error logging in:", error);
+    res.status(500).json({ error: error.message });
+  }
+};
 
-     const msg = {
-       from: '"the express app" <theexpressapp@example.com>',
-       to: `${email}`,
-       subject: "sup",
-       text: "Login time no see",
-     };
+const SendMail = async (req, res) => {
+  try {
+    console.log("Request body:", req.body);
+    const { email } = req.body;
+    console.log("Recipient email:", email);
 
-     const info = await transporter.sendMail(msg);
-     const token = jwt.sign({ email }, key, { expiresIn: "3h" });
-     console.log("Generated JWT token:", token);
-
-     console.log("Message sent: %s", info.messageId);
-
-     res.json({ status: "success", message: "Email sent successfully",
-     token : token
-    
+    const transporter = nodemailer.createTransport({
+      host: "server.internsbee.com",
+      port: 645,
+      auth: {
+        user: "internsbee@server.internsbee.com",
+        pass: "7PKBVkYd",
+      },
     });
-   } catch (error) {
-     console.error("Error sending email:", error);
-     res
-       .status(500)
-       .json({ status: "error", message: "Internal server error" });
-   }
 
-}
+    const msg = {
+      from: '"the express app" <theexpressapp@example.com>',
+      to: `vedant.0370@gmail.com`,
+      subject: "sup",
+      text: "Login time no see",
+    };
 
+    const info = await transporter.sendMail(msg);
+    const token = jwt.sign({ email }, key, { expiresIn: "3h" });
+    console.log("Generated JWT token:", token);
+
+    console.log("Message sent: %s", info.messageId);
+
+    res.json({
+      status: "success",
+      message: "Email sent successfully",
+      token: token,
+    });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ status: "error", message: "Internal server error" });
+  }
+};
 
 const getUser = async (req, res) => {
   try {
@@ -77,4 +100,13 @@ const getUser = async (req, res) => {
   }
 };
 
-module.exports = { creatUser , getUser , SendMail};
+const generateToken = async (req, res) => {
+  try {
+    const token = jwt.sign({ id: 1 }, key, { expiresIn: "3h" });
+    res.send(token);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { creatUser, getUser, SendMail, generateToken , Login };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,6 +7,8 @@ const userControl = require("../controller/userControl");
 
 router.post("/register", VerifyToken, userControl.creatUser);
 router.get("/users", VerifyToken, userControl.getUser);
-router.post("/sendmail",VerifyToken, userControl.SendMail);
+router.post("/sendmail", VerifyToken, userControl.SendMail);
+router.get("/token", userControl.generateToken);
+router.post("/login", userControl.Login);
 
 module.exports = router;
